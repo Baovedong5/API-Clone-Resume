e20065eb375653a7e0de25e27c0a9d0188735129
Phuong <lephuong10142002@gmail.com>
2023-10-05 20:50:55 +0700

    CRUD user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { AppService } from './app.service';
       inject: [ConfigService],
     }),
     ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/users.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import mongoose, { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { genSaltSync, hashSync } from 'bcryptjs';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  hashPassword = (password: string) => {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+    return hash;
+  };
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, password, name } = createUserDto;
+    const hashPassword = this.hashPassword(password);
+    const user = await this.userModel.create({
+      email,
+      password: hashPassword,
+      name,
+    });
+    return user;
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  async findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) return `not found`;
+    return await this.userModel.findOne({
+      _id: id,
+    });
+  }
+
+  async update(updateUserDto: UpdateUserDto) {
+    return await this.userModel.updateOne(
+      {
+        _id: updateUserDto._id,
+      },
+      { ...updateUserDto },
+    );
+  }
+
+  async remove(id: string) {
+    return await this.userModel.deleteOne({ _id: id });
+  }
+}
