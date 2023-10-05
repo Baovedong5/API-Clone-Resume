b2e5e76bddcb3be3f4edf6449247c67f4d6b1f4d
Phuong <lephuong10142002@gmail.com>
2023-10-05 16:00:53 +0700

    config env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+  ) {}
+
+  // private appService: AppService
+  // constructor(appService: AppService) {
+  //this.appService = appService
+  //}
 
   @Get()
   getHello(): string {
