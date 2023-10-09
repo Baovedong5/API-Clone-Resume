96d955a9e9268bbb1993fc0ad48deca5efcc97ea
Phuong <lephuong10142002@gmail.com>
2023-10-09 19:53:26 +0700

    CRUD Job

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CompaniesModule } from './companies/companies.module';
+import { JobsModule } from './jobs/jobs.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { CompaniesModule } from './companies/companies.module';
     UsersModule,
     AuthModule,
     CompaniesModule,
+    JobsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/jobs/jobs.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+
+import { JobsService } from './jobs.service';
+import { CreateJobDto } from './dto/create-job.dto';
+import { UpdateJobDto } from './dto/update-job.dto';
+import { Public, ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from 'src/users/users.interface';
+
+@Controller('jobs')
+export class JobsController {
+  constructor(private readonly jobsService: JobsService) {}
+
+  @Post()
+  @ResponseMessage('Create a new job')
+  async create(@Body() createJobDto: CreateJobDto, @User() user: IUser) {
+    let newJob = await this.jobsService.create(createJobDto, user);
+
+    return newJob;
+  }
+
+  @Get()
+  @ResponseMessage('Fetch user with pagination')
+  findAll(
+    @Query('current') currentPage: string,
+    @Query('pageSize') limit: string,
+    @Query() qs: string,
+  ) {
+    return this.jobsService.findAll(+currentPage, +limit, qs);
+  }
+
+  @Get(':id')
+  @ResponseMessage('Fetch a job by id')
+  findOne(@Param('id') id: string) {
+    return this.jobsService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ResponseMessage('Update a job')
+  update(
+    @Param('id') id: string,
+    @Body() updateJobDto: UpdateJobDto,
+    @User() user: IUser,
+  ) {
+    return this.jobsService.update(id, updateJobDto, user);
+  }
+
+  @Delete(':id')
+  @ResponseMessage('Delete a job')
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.jobsService.remove(id, user);
+  }
+}
