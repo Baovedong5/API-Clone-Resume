046b6b2d75cd73e2bc0fb3960914a5ef9c133d89
Phuong <lephuong10142002@gmail.com>
2023-10-11 20:44:33 +0700

    upload single file

---
+++ src/files/files.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+
+import { FilesService } from './files.service';
+import { FilesController } from './files.controller';
+import { MulterConfigService } from './multer.config';
+
+@Module({
+  imports: [
+    MulterModule.registerAsync({
+      useClass: MulterConfigService,
+    }),
+  ],
+  controllers: [FilesController],
+  providers: [FilesService],
+})
+export class FilesModule {}
